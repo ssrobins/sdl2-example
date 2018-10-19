36743595ecb9d5208b2b71a59e8cd42162b6650e
Steve Robinson <ssrobins@gmail.com>
2018-10-18 21:09:06 -0700

    Bring build system updates from my game to this project

--- copy_files_to_binary_dir.cmake
+++ copy_files_to_binary_dir.cmake
@@ -52,6 +52,22 @@ if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/assets)
     endif()
 endif()
 
+# Stage Mac bundle icon
+if(APPLE AND NOT IOS)
+    if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/icon.icns)
+        get_target_property(is_mac_bundle ${target_name} MACOSX_BUNDLE)
+        if(${is_mac_bundle})
+            add_custom_command(
+                TARGET ${target_name}
+                POST_BUILD
+                COMMAND cmake -E copy
+                    ${CMAKE_CURRENT_SOURCE_DIR}/icon.icns
+                    ${CMAKE_CURRENT_BINARY_DIR}/${config}/${target_name}.app/Contents/Resources/icon.icns
+            )
+        endif()
+    endif()
+endif()
+
 if(ANDROID)
     # Stage copy of gradle project for Android build and SDL's Java files
     execute_process(
@@ -63,14 +79,41 @@ if(ANDROID)
             ${CMAKE_CURRENT_BINARY_DIR}/Android/app/src/main/java/org/libsdl/app
     )
 
+    if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/assets_dontship/Android)
+        execute_process(
+            COMMAND ${CMAKE_COMMAND} -E copy
+                ${CMAKE_CURRENT_SOURCE_DIR}/assets_dontship/Android/icon_48x48.png
+                ${CMAKE_CURRENT_BINARY_DIR}/Android/app/src/main/res/mipmap-mdpi/ic_launcher.png
+            COMMAND ${CMAKE_COMMAND} -E copy
+                ${CMAKE_CURRENT_SOURCE_DIR}/assets_dontship/Android/icon_72x72.png
+                ${CMAKE_CURRENT_BINARY_DIR}/Android/app/src/main/res/mipmap-hdpi/ic_launcher.png
+            COMMAND ${CMAKE_COMMAND} -E copy
+                ${CMAKE_CURRENT_SOURCE_DIR}/assets_dontship/Android/icon_96x96.png
+                ${CMAKE_CURRENT_BINARY_DIR}/Android/app/src/main/res/mipmap-xhdpi/ic_launcher.png
+            COMMAND ${CMAKE_COMMAND} -E copy
+                ${CMAKE_CURRENT_SOURCE_DIR}/assets_dontship/Android/icon_144x144.png
+                ${CMAKE_CURRENT_BINARY_DIR}/Android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png
+            COMMAND ${CMAKE_COMMAND} -E copy
+                ${CMAKE_CURRENT_SOURCE_DIR}/assets_dontship/Android/icon_192x192.png
+                ${CMAKE_CURRENT_BINARY_DIR}/Android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
+            COMMAND ${CMAKE_COMMAND} -E copy
+                ${CMAKE_CURRENT_SOURCE_DIR}/assets_dontship/Android/icon_512x512.png
+                ${CMAKE_CURRENT_BINARY_DIR}/Android/app/src/main/ic_launcher-web.png
+        )
+    endif()
+
     # Process files so they include target-specific properties
+    configure_file (
+        ${CMAKE_SOURCE_DIR}/Android/app/src/main/AndroidManifest.xml
+        ${CMAKE_CURRENT_BINARY_DIR}/Android/app/src/main/AndroidManifest.xml
+    )
     configure_file (
         ${CMAKE_SOURCE_DIR}/Android/app/build.gradle
         ${CMAKE_CURRENT_BINARY_DIR}/Android/app/build.gradle
     )
     configure_file (
-        ${CMAKE_SOURCE_DIR}/Android/app/src/main/java/com/my/app/MainActivity.java
-        ${CMAKE_CURRENT_BINARY_DIR}/Android/app/src/main/java/com/my/app/MainActivity.java
+        ${CMAKE_SOURCE_DIR}/Android/templates/MainActivity.java
+        ${CMAKE_CURRENT_BINARY_DIR}/Android/app/src/main/java/com/${company}/${target_name}/MainActivity.java
     )
     configure_file (
         ${CMAKE_SOURCE_DIR}/Android/app/src/main/res/values/strings.xml
