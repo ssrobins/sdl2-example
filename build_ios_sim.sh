d8a265b0a0a03f4603ea11f61ff72150f35b474a
Steve Robinson <ssrobins@gmail.com>
2018-11-14 20:06:51 -0800

    Move build directories inside of the source tree

--- build_ios_sim.sh
+++ build_ios_sim.sh
@@ -1,14 +1,11 @@
 set -e
 
-build_name=ios-sim
+build_dir=build_ios_sim
 
 cd $(dirname "$0")
-source_dir=${PWD##*/} 
-build_dir=$source_dir-build-$build_name
-
-cd ..
 mkdir -p $build_dir
 cd $build_dir
-conan install ../$source_dir -s os=iOS -s os.version=8.0
-cmake -G Xcode -DCMAKE_TOOLCHAIN_FILE=../$source_dir/ios.toolchain.cmake -DENABLE_BITCODE=FALSE -DIOS_PLATFORM=SIMULATOR64 ../$source_dir
+
+conan install .. -s os=iOS -s os.version=8.0
+cmake -G Xcode -DCMAKE_TOOLCHAIN_FILE=../ios.toolchain.cmake -DENABLE_BITCODE=FALSE -DIOS_PLATFORM=SIMULATOR64 ..
 cmake --build . --config Release
