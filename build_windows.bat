85b824b3c7335c9bc89761e134b8b78e79c8b4d7
Steve Robinson <ssrobins@gmail.com>
2018-11-16 18:38:37 -0800

    Refinements in Windows batch script

--- build_windows.bat
+++ build_windows.bat
@@ -1,16 +1,15 @@
 @echo off
+setlocal
 
 set build_dir=build_windows
 
-pushd %~dp0
-mkdir %build_dir%
-pushd %build_dir%
+cd /d %~dp0
+if not exist %build_dir% mkdir %build_dir%
+cd %build_dir%
 
 conan install .. -s arch=x86 -s compiler.runtime=MT || goto :error
 cmake .. || goto :error
 cmake --build . --config Release --target PACKAGE -- /m || goto :error
 
 :error
-popd
-popd
 exit /b %errorlevel%
