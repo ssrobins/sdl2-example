f51271f98f02927adb3f7c8a6787038e33edc91e
Steve Robinson <ssrobins@gmail.com>
2019-01-13 10:30:59 -0800

    Use Python for conanfile because it's more flexible

---
+++ conanfile.py
@@ -0,0 +1,9 @@
+from conans import ConanFile
+
+class Conan(ConanFile):
+
+    settings = "os"
+    generators = "cmake"
+
+    def requirements(self):
+        self.requires.add("sdl2/2.0.8@stever/testing")
