59c7dccd1fb2904ac63979ecec3e34c5d8dba9a8
Steve Robinson <ssrobins@gmail.com>
2019-01-23 22:23:11 -0800

    Use stable packages

--- conanfile.py
+++ conanfile.py
@@ -6,4 +6,4 @@ class Conan(ConanFile):
     generators = "cmake"
 
     def requirements(self):
-        self.requires.add("sdl2/2.0.8@stever/testing")
+        self.requires.add("sdl2/2.0.8@stever/stable")
