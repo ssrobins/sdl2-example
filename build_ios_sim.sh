9dfc0b32a43e75c76c5557833e9d01f8fce2a1fe
Steve Robinson <ssrobins@gmail.com>
2019-01-26 22:17:09 -0800

    CMake reorg

--- build_ios_sim.sh
+++ build_ios_sim.sh
@@ -7,5 +7,5 @@ mkdir -p $build_dir
 cd $build_dir
 
 conan install .. -s os=iOS -s os.version=8.0
-cmake -G Xcode -DCMAKE_TOOLCHAIN_FILE=../ios.toolchain.cmake -DENABLE_BITCODE=FALSE -DIOS_PLATFORM=SIMULATOR64 ..
+cmake -G Xcode -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DENABLE_BITCODE=FALSE -DIOS_PLATFORM=SIMULATOR64 ..
 cmake --build . --config Release
