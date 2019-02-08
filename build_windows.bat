6796b9970ee3f015f8f0722d04d404611f2818a5
Steve Robinson <ssrobins@gmail.com>
2019-02-07 19:48:01 -0800

    Explicitly set Visual Studio version so I can continue building with VS2017 even though I have VS2019 installed

--- build_windows.bat
+++ build_windows.bat
@@ -7,8 +7,8 @@ cd /d %~dp0
 if not exist %build_dir% mkdir %build_dir%
 cd %build_dir%
 
-conan install .. -s arch=x86 -s compiler.runtime=MT || goto :error
-cmake .. || goto :error
+conan install .. -s arch=x86 -s compiler.version=15 -s compiler.runtime=MT || goto :error
+cmake -G "Visual Studio 15 2017" .. || goto :error
 cmake --build . --config Release --target PACKAGE -- /m || goto :error
 
 :error
