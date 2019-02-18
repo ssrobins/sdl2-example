db5a7582fcaa304d4269ac8aad88046f9438b9f0
Steve Robinson <ssrobins@gmail.com>
2019-02-18 11:25:57 -0800

    Support add arm64 support for iOS

--- build_ios.sh
+++ build_ios.sh
@@ -7,5 +7,5 @@ mkdir -p $build_dir
 cd $build_dir
 
 conan install .. -s os=iOS -s arch=armv7 -s os.version=8.0
-cmake -G Xcode -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DIOS_ARCH=armv7 -DENABLE_BITCODE=FALSE ..
+cmake -G Xcode -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DIOS_ARCH="armv7 arm64" -DENABLE_BITCODE=FALSE ..
 cmake --build . --config Release
