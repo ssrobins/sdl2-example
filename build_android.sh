4ee7245d226f3ec1297bb2daebed89812c40410c
Steve Robinson <ssrobins@gmail.com>
2019-03-01 04:39:58 +0000

    Upgrade Android NDK to 19b

--- build_android.sh
+++ build_android.sh
@@ -6,6 +6,8 @@ cd $(dirname "$0")
 mkdir -p $build_dir
 cd $build_dir
 
-conan install .. -s os=Android -s os.api_level=${android_sdk_version} -s arch=armv7 -s compiler=clang -s compiler.version=7.0 -s compiler.libcxx=libstdc++11
-cmake -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=${android_sdk_version} -DCMAKE_ANDROID_ARCH_ABI=${android_arch_abi} -DCMAKE_ANDROID_NDK=$ANDROID_HOME/android-ndk-${android_ndk_version} -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_BUILD_TYPE=Release ..
+conan install .. -s os=Android -s os.api_level=${android_sdk_version} -s arch=armv7 -s compiler=clang -s compiler.version=8 -s compiler.libcxx=libc++
+
+cmake -DANDROID_ABI=${android_arch_abi} -DANDROID_TOOLCHAIN=clang -DANDROID_STL=c++_static -DCMAKE_TOOLCHAIN_FILE=$ANDROID_HOME/android-ndk-${android_ndk_version}/build/cmake/android.toolchain.cmake -DCMAKE_BUILD_TYPE=Release ..
+
 cmake --build .
