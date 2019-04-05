1e14e8c340f67a8ff7906a4ba8528c2ef1c78568
Steve Robinson <ssrobins@gmail.com>
2019-04-04 22:14:56 -0700

    Upgrade to VS2019

--- build_windows.bat
+++ build_windows.bat
@@ -7,9 +7,9 @@ cd /d %~dp0
 if not exist %build_dir% mkdir %build_dir%
 cd %build_dir%
 
-conan install .. -s arch=x86 -s compiler.version=15 -s compiler.runtime=MT || goto :error
+conan install .. -s arch=x86 -s compiler.version=16 -s compiler.runtime=MT || goto :error
 
-cmake -G "Visual Studio 15 2017" .. || goto :error
+cmake -G "Visual Studio 16 2019" -A Win32 .. || goto :error
 
 cmake --build . --config Release --target PACKAGE -- /m || goto :error
 
