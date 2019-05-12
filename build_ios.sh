f9404c909a541f4c591f6332e01e7cdd31a2dff4
Steve Robinson <ssrobins@gmail.com>
2019-05-11 22:17:23 -0700

    Use the built-in iOS toolchain instead of a third-party one

--- build_ios.sh
+++ build_ios.sh
@@ -8,6 +8,6 @@ cd $build_dir
 
 conan install .. -s os=iOS -s arch=armv7 -s os.version=8.0
 
-cmake -G Xcode -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DIOS_ARCH="armv7 arm64" -DENABLE_BITCODE=FALSE ..
+cmake -G Xcode -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_ARCHITECTURES="armv7 arm64" -DCMAKE_OSX_DEPLOYMENT_TARGET=8.0 -DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY ..
 
 cmake --build . --config Release
