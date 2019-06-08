0de772f05d92ed4c6ab62ee0f44e696977e768af
Steve Robinson <ssrobins@gmail.com>
2019-06-08 09:39:18 -0700

    Set Android 'api_level' to minSdkVersion so Conan sets the --target flag properly

--- build_android-v8a.sh
+++ build_android-v8a.sh
@@ -6,8 +6,10 @@ cd $(dirname "$0")
 mkdir -p $build_dir
 cd $build_dir
 
-conan install .. -s os=Android -s os.api_level=${android_sdk_version} -s arch=armv8 -s compiler=clang -s compiler.version=8 -s compiler.libcxx=libc++
+export android_min_sdk_version=21
 
-cmake -DANDROID_ABI=arm64-v8a -DANDROID_TOOLCHAIN=clang -DANDROID_STL=c++_static -DCMAKE_TOOLCHAIN_FILE=$ANDROID_HOME/android-ndk-${android_ndk_version}/build/cmake/android.toolchain.cmake -DCMAKE_BUILD_TYPE=Release ..
+conan install .. -s os=Android -s os.api_level=$android_min_sdk_version -s arch=armv8 -s compiler=clang -s compiler.version=8 -s compiler.libcxx=libc++
+
+cmake -DANDROID_PLATFORM=android-$android_min_sdk_version -DANDROID_ABI=arm64-v8a -DANDROID_TOOLCHAIN=clang -DANDROID_STL=c++_static -DCMAKE_TOOLCHAIN_FILE=$ANDROID_HOME/android-ndk-${android_ndk_version}/build/cmake/android.toolchain.cmake -DCMAKE_BUILD_TYPE=Release ..
 
 cmake --build .
