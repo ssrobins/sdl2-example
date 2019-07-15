d8b63b1a3515dafdad045f0e924b3a7cfb636f83
Steve Robinson <ssrobins@gmail.com>
2019-07-14 18:13:39 -0700

    Use CPack to package the Android APKs

--- build_android-v8a.sh
+++ build_android-v8a.sh
@@ -12,4 +12,4 @@ conan install .. -s os=Android -s os.api_level=$android_min_sdk_version -s arch=
 
 cmake -DANDROID_PLATFORM=android-$android_min_sdk_version -DANDROID_ABI=arm64-v8a -DANDROID_TOOLCHAIN=clang -DANDROID_STL=c++_static -DCMAKE_TOOLCHAIN_FILE=$ANDROID_HOME/android-ndk-${android_ndk_version}/build/cmake/android.toolchain.cmake -DCMAKE_BUILD_TYPE=Release ..
 
-cmake --build .
+cmake --build . --target package -- -j 4
