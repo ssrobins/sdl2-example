a1f68718219b8862d7e20b99517bfb92ffa9a8a1
Steve Robinson <ssrobins@gmail.com>
2019-08-14 20:31:48 -0700

    Upgrade gcc

--- conanfile.py
+++ conanfile.py
@@ -6,4 +6,4 @@ class Conan(ConanFile):
     generators = "cmake"
 
     def requirements(self):
-        self.requires.add("sdl2/2.0.8#ece35a6c4011a59b0240d62bccb7a469ac47d6b9")
+        self.requires.add("sdl2/2.0.8#69c21e066605654a91aa48560889ada1b5e3913a")
