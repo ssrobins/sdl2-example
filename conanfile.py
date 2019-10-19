5616bb69273933bb71a967264767ac005bee2fb9
Steve Robinson <ssrobins@gmail.com>
2019-10-18 18:12:31 -0700

    Upgrade Gradle

--- conanfile.py
+++ conanfile.py
@@ -6,4 +6,4 @@ class Conan(ConanFile):
     generators = "cmake"
 
     def requirements(self):
-        self.requires.add("ssrobins_engine/0.1.0#b89fc804e84394548b432a2912fd2d567f4ac2e8")
+        self.requires.add("ssrobins_engine/0.1.0#bfdd7e61f2ae28bfee82720e4b9e9b39ff8da27d")
