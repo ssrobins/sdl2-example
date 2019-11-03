f1a81e0bc2d31e5f37237b9b1c04a9dcd9216f78
Steve Robinson <ssrobins@gmail.com>
2019-11-03 14:21:00 -0800

    Upgrade Android SDK API

--- conanfile.py
+++ conanfile.py
@@ -8,5 +8,5 @@ class Conan(ConanFile):
     def requirements(self):
         if self.settings.os == "Android":
             self.requires.add("android_sdl2/0.1.0#99ca5d8f13de93355ccabef47eb90c6343d800f7")
-        self.requires.add("cmake_utils/0.1.0#7f17deeced79eecd4a03ba2d327bee3e5e794732")
+        self.requires.add("cmake_utils/0.1.0#9efedba7676c460adbfbf3a98e4c51cea7d320db")
         self.requires.add("sdl2/2.0.8#e429f599c8c7350ba1edb0e501cfb81c23df0e84")
