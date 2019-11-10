5335d9f909db4fc716f4aefb5aa11fe72bf21c20
Steve Robinson <ssrobins@gmail.com>
2019-11-09 21:48:04 -0800

    Move cmake_utils to build_requirements

--- conanfile.py
+++ conanfile.py
@@ -1,12 +1,13 @@
 from conans import ConanFile
 
 class Conan(ConanFile):
-
     settings = "os"
     generators = "cmake"
 
-    def requirements(self):
+    def build_requirements(self):
         if self.settings.os == "Android":
-            self.requires.add("android_sdl2/0.1.0#99ca5d8f13de93355ccabef47eb90c6343d800f7")
-        self.requires.add("cmake_utils/0.1.0#9efedba7676c460adbfbf3a98e4c51cea7d320db")
+            self.build_requires.add("android_sdl2/0.1.0#99ca5d8f13de93355ccabef47eb90c6343d800f7")
+        self.build_requires.add("cmake_utils/0.1.0#9efedba7676c460adbfbf3a98e4c51cea7d320db")
+
+    def requirements(self):
         self.requires.add("sdl2/2.0.8#e429f599c8c7350ba1edb0e501cfb81c23df0e84")
