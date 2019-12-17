4f30fb3a37a90a78ed54973992afc36544b8f37a
Steve Robinson <ssrobins@gmail.com>
2019-12-17 03:42:29 +0000

    Run Conan inside CMake. Add CMakeSettings.json so the project folder can be opened in VS2019.

--- build_linux.sh
+++ build_linux.sh
@@ -6,10 +6,6 @@ cd $(dirname "$0")
 mkdir -p $build_dir
 cd $build_dir
 
-conan remote add bintray-stever https://api.bintray.com/conan/stever/conan --insert --force
-
-conan install --update .. -s compiler.libcxx=libstdc++11
-
 config=Release
 
 cmake -G Ninja -DCMAKE_BUILD_TYPE=$config ..
