c62da115a52ef1804697ae840e3799f8c7ffbbeb
Steve Robinson <ssrobins@gmail.com>
2020-01-01 15:05:19 -0800

    Use modern CMake to set -g0 on Android Release builds

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ class Conan(ConanFile):
     def build_requirements(self):
         if self.settings.os == "Android":
             self.build_requires.add("android_sdl2/0.1.0#debda07d9c722b256dc83beccc487d30fffbd274")
-        self.build_requires.add("cmake_utils/0.3.1#1cf9333e6fba1b7350ec8d4d06f737b54d163eef")
+        self.build_requires.add("cmake_utils/0.3.1#95dea75496ef60374382c194489e6524e9503eb4")
 
     def requirements(self):
         self.requires.add("sdl2/2.0.8#0ac39a5179e1f92a0af645ee15c70760af8cf590")
