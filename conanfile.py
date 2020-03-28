c663d70875ae6c7318b2fd8984d8c5dfc6a28df0
Steve Robinson <ssrobins@gmail.com>
2020-03-28 00:41:32 -0700

    Upgrade Gradle and get assets folder copied to the proper location for mult-config Ninja Android builds

--- conanfile.py
+++ conanfile.py
@@ -6,8 +6,8 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires("android_sdl2/0.1.0#5ce8c1a310ba03a40c1b8e3f5059eecfcb287e30")
-        self.build_requires("cmake_utils/0.3.1#d093c585be2418d6a664babbec39e71d6b5cd11d")
+            self.build_requires("android_sdl2/0.1.0#1aea09bd255829b7b78634e74c7fd296d99e4582")
+        self.build_requires("cmake_utils/0.3.1#b92e3b563e31a4fe0e55849f3bfdb55eb7b06284")
 
     def requirements(self):
         self.requires("sdl2/2.0.8#ecad87bc0bf63f851560e29d3b2e0d1881051544")
