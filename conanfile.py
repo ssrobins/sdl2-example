f354d2b06409ac1e28f35761f853e6d5e1960d48
Steve Robinson <ssrobins@gmail.com>
2020-07-25 14:16:05 -0700

    Replace bash and batch build scripts with a single Python script. Check for and use environment variables for the Android NDK and SDK paths.

--- conanfile.py
+++ conanfile.py
@@ -6,8 +6,8 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires("android_sdl2/0.1.0#1aea09bd255829b7b78634e74c7fd296d99e4582")
+            self.build_requires("android_sdl2/0.1.0#732c8c9ca19314c41111a1ea1a339bb1ed6cfdea")
         self.build_requires("cmake_utils/0.3.1#b92e3b563e31a4fe0e55849f3bfdb55eb7b06284")
 
     def requirements(self):
-        self.requires("sdl2/2.0.8#ecad87bc0bf63f851560e29d3b2e0d1881051544")
+        self.requires("sdl2/2.0.8#cfc3fac0c9f98475f888516718b49146cc9dc8ba")
