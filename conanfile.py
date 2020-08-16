09199a49002c24f440d775217caf4121b757f18e
Steve Robinson <ssrobins@gmail.com>
2020-08-16 12:02:02 -0700

    Fix CPack-based Gradle build

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ class Conan(ConanFile):
     def build_requirements(self):
         if self.settings.os == "Android":
             self.build_requires("android_sdl2/0.1.0#1d2ac253f9887def0813161c99f252f08e127f1f")
-        self.build_requires("cmake_utils/0.3.1#9ce128bcf426b83f42e04b45a26d16d7a138867d")
+        self.build_requires("cmake_utils/0.3.1#9ea936d419d454af4927845a23b913923ceaa29a")
 
     def requirements(self):
         self.requires("sdl2/2.0.8#cfc3fac0c9f98475f888516718b49146cc9dc8ba")
