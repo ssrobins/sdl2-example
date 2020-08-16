dcbe4885c4165d40cadee0e51e50345b5d8c44f1
Steve Robinson <ssrobins@gmail.com>
2020-08-16 10:46:02 -0700

    Run CMake build in Gradle and consolidate Android Studio projects to one top-level directory

--- conanfile.py
+++ conanfile.py
@@ -6,8 +6,8 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires("android_sdl2/0.1.0#cd160eec998fd3e7fbe292deb337c97219edb896")
-        self.build_requires("cmake_utils/0.3.1#b92e3b563e31a4fe0e55849f3bfdb55eb7b06284")
+            self.build_requires("android_sdl2/0.1.0#1d2ac253f9887def0813161c99f252f08e127f1f")
+        self.build_requires("cmake_utils/0.3.1#9ce128bcf426b83f42e04b45a26d16d7a138867d")
 
     def requirements(self):
         self.requires("sdl2/2.0.8#cfc3fac0c9f98475f888516718b49146cc9dc8ba")
