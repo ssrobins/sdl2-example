54d46b887fae9220e4cfd875766992f6b866cfdc
Steve Robinson <ssrobins@gmail.com>
2020-09-21 22:45:51 -0700

    Upgrade to Xcode 12

--- conanfile.py
+++ conanfile.py
@@ -6,8 +6,8 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires("android_sdl2/0.1.0#1d2ac253f9887def0813161c99f252f08e127f1f")
-        self.build_requires("cmake_utils/0.3.1#a680a99b588c0c874b8ee9af304acf30aa3b5787")
+            self.build_requires("android_sdl2/0.1.0#33d7832e580dafa05810540f2808f3b070286189")
+        self.build_requires("cmake_utils/0.3.1#217df94bdd79aaa9b2aec1a8bd8b4eca73411f25")
 
     def requirements(self):
-        self.requires("sdl2/2.0.8#cfc3fac0c9f98475f888516718b49146cc9dc8ba")
+        self.requires("sdl2/2.0.8#b16a079e0dd4a2a068a287212aaf9ec0cfaa1fa0")
