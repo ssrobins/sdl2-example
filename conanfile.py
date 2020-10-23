76867fac88b6f9880f37cbbc8b30f3c4a6164e1d
Steve Robinson <ssrobins@gmail.com>
2020-10-22 22:58:04 -0700

    Update package

--- conanfile.py
+++ conanfile.py
@@ -10,4 +10,4 @@ class Conan(ConanFile):
         self.build_requires("cmake_utils/0.3.1#cc144db607f04d12c0b18303a7c7d37386ce0783")
 
     def requirements(self):
-        self.requires("sdl2/2.0.8#7aeaa31616b717a1fd7799edc88e95c8c03e3af1")
+        self.requires("sdl2/2.0.8#85acaf572b4af7f9846d0738717924e3ba878607")
