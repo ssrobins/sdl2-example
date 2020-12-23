7da2ea9383627169b205cb89fe2fcd87a72ee6d1
Steve Robinson <ssrobins@gmail.com>
2020-12-23 00:24:10 -0800

    Upgrade Android NDK

--- conanfile.py
+++ conanfile.py
@@ -6,8 +6,8 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires("android_sdl2/0.1.0#080a2227eb3904d6c6df6bd90ff689d1cf12736e")
+            self.build_requires("android_sdl2/0.1.0#b185e9d8c14eef5fcbce6e9fb0468c944149a4c5")
         self.build_requires("cmake_utils/0.3.1#cc144db607f04d12c0b18303a7c7d37386ce0783")
 
     def requirements(self):
-        self.requires("sdl2/2.0.8#abbd4c5be105743339ebff6140b9154669b9de00")
+        self.requires("sdl2/2.0.8#f45c74e900f09457e388645638c72802bd4cd081")
