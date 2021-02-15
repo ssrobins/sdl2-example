aa2b6f92f195e6c5de9c2af39b5e4276d534aa2b
Steve Robinson <ssrobins@gmail.com>
2021-02-14 20:43:25 -0800

    Switch from bintray to artifactory for Conan packages

--- conanfile.py
+++ conanfile.py
@@ -6,8 +6,8 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires("android_sdl2/0.1.0#b185e9d8c14eef5fcbce6e9fb0468c944149a4c5")
-        self.build_requires("cmake_utils/0.3.1#cc144db607f04d12c0b18303a7c7d37386ce0783")
+            self.build_requires("android_sdl2/0.1.0#ed934365b888c490f9dca54779d85a6e3cc3cc52")
+        self.build_requires("cmake_utils/0.3.1#e474aafdec36cf92d97e781b844f390f3170f29f")
 
     def requirements(self):
-        self.requires("sdl2/2.0.14#766b7457c4be1aa4a5f59052374658437c2ca691")
+        self.requires("sdl2/2.0.14#f467b416e317c6dae86ecfd38fbf5c2ab2072a2c")
