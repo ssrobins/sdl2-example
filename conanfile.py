abc4ed2599d4d2fb4b1df9b32a8f5cef72d4b249
Steve Robinson <ssrobins@gmail.com>
2021-03-26 23:22:38 -0700

    Upgrade Android NDK, gcc, and Xcode

--- conanfile.py
+++ conanfile.py
@@ -6,8 +6,8 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires("android_sdl2/0.1.0#ed934365b888c490f9dca54779d85a6e3cc3cc52")
-        self.build_requires("cmake_utils/0.3.1#e474aafdec36cf92d97e781b844f390f3170f29f")
+            self.build_requires("android_sdl2/0.1.0#a8e9c63ad2763313865f84a027b0509ab6a4208f")
+        self.build_requires("cmake_utils/0.3.1#da30d52b2c5db13fc90a22140f704d67c7635319")
 
     def requirements(self):
-        self.requires("sdl2/2.0.14#f467b416e317c6dae86ecfd38fbf5c2ab2072a2c")
+        self.requires("sdl2/2.0.14#e219925dea4ac723af26595deffea181936f9b2a")
