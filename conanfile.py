ca4fc7f75b0e7f8d95d59cf1255d42291079453f
Steve Robinson <ssrobins@gmail.com>
2021-07-11 18:15:00 -0700

    Use compiler.libcxx=c++_static for Android and include compiler name/version for Linux

--- conanfile.py
+++ conanfile.py
@@ -10,4 +10,4 @@ class Conan(ConanFile):
         self.build_requires("cmake_utils/0.3.1#77d5f06b9b20302a5410e41ed45e7bbea7de90a5")
 
     def requirements(self):
-        self.requires("sdl2/2.0.14#7d63f39c4feb6f6922d7e88c834c2fbfd9d586c7")
+        self.requires("sdl2/2.0.14#0f6e34b34f9f3572720069184d50800ef1130aa0")
