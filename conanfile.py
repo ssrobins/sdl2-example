272b014c65f45d2ebfe4f93277d2c13d1216ad2d
Steve Robinson <ssrobins@gmail.com>
2021-09-30 21:04:39 -0700

    Build Linux with Ubuntu 18

--- conanfile.py
+++ conanfile.py
@@ -6,8 +6,8 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires("android_sdl2/0.1.0#d299a2e1a9b1b779eb0e2bd7c11d2cf845e5f403")
+            self.build_requires("android_sdl2/0.1.0#ce144c60759faca04793da51b167a15d56b8d474")
         self.build_requires("cmake_utils/0.3.1#77d5f06b9b20302a5410e41ed45e7bbea7de90a5")
 
     def requirements(self):
-        self.requires("sdl2/2.0.16#b5081764744dd13a648d8ae2ff0d0abce3d7fe91")
+        self.requires("sdl2/2.0.16#558cc0716f686f17d95ee3f68ee18c240a79b20b")
