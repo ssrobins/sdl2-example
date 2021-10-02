36b48c16d30948d2a97230c9e26315e57fccf180
Steve Robinson <ssrobins@gmail.com>
2021-10-01 23:51:20 -0700

    Fix conan packages

--- conanfile.py
+++ conanfile.py
@@ -6,8 +6,8 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires("android_sdl2/0.1.0#ce144c60759faca04793da51b167a15d56b8d474")
-        self.build_requires("android_sdl2/0.1.0#b05721a9fb7f10bd7ae3392924dcf58aa497abb9")
+            self.build_requires("android_sdl2/0.1.0#b05721a9fb7f10bd7ae3392924dcf58aa497abb9")
+        self.build_requires("cmake_utils/0.3.1#77d5f06b9b20302a5410e41ed45e7bbea7de90a5")
 
     def requirements(self):
         self.requires("sdl2/2.0.16#558cc0716f686f17d95ee3f68ee18c240a79b20b")
