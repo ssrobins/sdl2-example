b90a3c2afced9a86fe39de114f9c1d6275671a17
Steve Robinson <ssrobins@gmail.com>
2021-10-31 11:47:41 -0700

    Use built-in CMake commands to copy assets to macOS and iOS packages

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ class Conan(ConanFile):
     def build_requirements(self):
         if self.settings.os == "Android":
             self.build_requires("android_sdl2/0.1.0#b276f95f39b2d5afe404751198b15c400938844d")
-        self.build_requires("cmake_utils/0.3.1#886de25a0b74bb31dd186971730d24bae92546d6")
+        self.build_requires("cmake_utils/0.3.1#2e2e0de4635e8c3a83655a207c63ad34ba1dadd7")
 
     def requirements(self):
         self.requires("sdl2/2.0.16#e48e0f700a2932b3c19dcc83fac49275cf8d5efc")
