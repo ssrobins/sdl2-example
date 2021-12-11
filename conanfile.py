eb364716f1619720358597a5b1f3eddb282eaa65
Steve Robinson <ssrobins@gmail.com>
2021-12-11 11:57:20 -0800

    Skip iOS build if packaging is selected to avoid building multiple times

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ class Conan(ConanFile):
     def build_requirements(self):
         if self.settings.os == "Android":
             self.build_requires("android_sdl2/2.1.0#4631baf96c5937b22753e9ac1146265ca3b32aff")
-        self.build_requires("cmake_utils/5.0.0#1ecfed8c68a43ea17d321701cc8a91df21d06453")
+        self.build_requires("cmake_utils/5.1.0#2c0d8f9dda3cac137976849bb3851fd6c4999de0")
 
     def requirements(self):
         self.requires("sdl2/2.0.18#f9abc4b1f200e751c41e1aab3026eeb7370b0d46")
