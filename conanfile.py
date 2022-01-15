b50760745284a58dcf4e3d866f400a86d44ab2c3
Steve Robinson <ssrobins@gmail.com>
2022-01-15 11:48:07 -0800

    Use asset catalog for macOS icons (#10)
    * Remove old iOS icons
    
    * Add new iOS and macOS icons
    
    * Configure macOS to use xcassets for the icons
    
    * Make updates to CMake icon code
    
    * Update product version

--- conanfile.py
+++ conanfile.py
@@ -6,8 +6,8 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires("android_sdl2/2.3.0#9517496737c184f98924eec823064964c8123474")
-        self.build_requires("cmake_utils/5.1.0#2c0d8f9dda3cac137976849bb3851fd6c4999de0")
+            self.build_requires("android_sdl2/2.4.0#41295dd86e5d152269019ea03930766db0c0bddc")
+        self.build_requires("cmake_utils/6.0.0#ac1a71244046f44f5484a554918f6a42a1f86d99")
 
     def requirements(self):
         self.requires("sdl2/2.0.18#f9abc4b1f200e751c41e1aab3026eeb7370b0d46")
