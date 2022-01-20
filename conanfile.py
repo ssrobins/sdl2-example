363690a53445653f2abe403d4cbd8b8d46b1e6dc
Steve Robinson <ssrobins@gmail.com>
2022-01-19 20:29:30 -0800

    Upgrade SDL2

--- conanfile.py
+++ conanfile.py
@@ -10,4 +10,4 @@ class Conan(ConanFile):
         self.build_requires("cmake_utils/6.0.0#ac1a71244046f44f5484a554918f6a42a1f86d99")
 
     def requirements(self):
-        self.requires("sdl2/2.0.18#f9abc4b1f200e751c41e1aab3026eeb7370b0d46")
+        self.requires("sdl2/2.0.20#45420ef8e58422639bfab3f61e40d75a03091154")
