1830bc4b52cadfbe15f4cc416fac389d3ee022ed
Steve Robinson <ssrobins@gmail.com>
2022-02-09 22:04:51 -0800

    Upgrade Android target SDK and gradle plugin

--- conanfile.py
+++ conanfile.py
@@ -6,8 +6,8 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires("android_sdl2/2.4.0#41295dd86e5d152269019ea03930766db0c0bddc")
-        self.build_requires("cmake_utils/6.0.0#ac1a71244046f44f5484a554918f6a42a1f86d99")
+            self.build_requires("android_sdl2/2.6.0#508b1df8b9b4b37172e71af0c64acb23a413e3c9")
+        self.build_requires("cmake_utils/6.0.1#c04990b3e6de61a45c1b7a2845a7aa6daf2abf9d")
 
     def requirements(self):
         self.requires("sdl2/2.0.20#45420ef8e58422639bfab3f61e40d75a03091154")
