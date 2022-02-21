a810dc20e795a74369b731b456df550b36b0a8b0
Steve Robinson <ssrobins@gmail.com>
2022-02-21 00:27:01 -0800

    Set PRODUCT_BUNDLE_IDENTIFIER to fix macOS/iOS build warning

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ class Conan(ConanFile):
     def build_requirements(self):
         if self.settings.os == "Android":
             self.build_requires("android_sdl2/2.6.0#508b1df8b9b4b37172e71af0c64acb23a413e3c9")
-        self.build_requires("cmake_utils/6.0.1#c04990b3e6de61a45c1b7a2845a7aa6daf2abf9d")
+        self.build_requires("cmake_utils/6.1.0#9ced9bcfd95178b35d2ec5955b725a5652dbda26")
 
     def requirements(self):
         self.requires("sdl2/2.0.20#45420ef8e58422639bfab3f61e40d75a03091154")
