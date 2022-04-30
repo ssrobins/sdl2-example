c889d66289ca8dfc96cdfbc38f02991fe01bb421
Steve Robinson <ssrobins@gmail.com>
2022-04-30 10:06:12 -0700

    Upgrade SDL2 (#11)

--- conanfile.py
+++ conanfile.py
@@ -6,8 +6,8 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires("android_sdl2/2.6.0#508b1df8b9b4b37172e71af0c64acb23a413e3c9")
+            self.build_requires("android_sdl2/2.6.2#da38ed876673a6da88d73f6dcb0138de5561125b")
         self.build_requires("cmake_utils/6.1.0#9ced9bcfd95178b35d2ec5955b725a5652dbda26")
 
     def requirements(self):
-        self.requires("sdl2/2.0.20#45420ef8e58422639bfab3f61e40d75a03091154")
+        self.requires("sdl2/2.0.22#818215441000572b96461f775dce5f47cdf15761")
