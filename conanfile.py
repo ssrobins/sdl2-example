d87ca2728b366685c033af5723ef88c90769758a
Steve Robinson <ssrobins@gmail.com>
2022-05-23 10:53:56 -0700

    Clean up

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ class Conan(ConanFile):
     def build_requirements(self):
         if self.settings.os == "Android":
             self.build_requires("android_sdl2/2.6.2#da38ed876673a6da88d73f6dcb0138de5561125b")
-        self.build_requires("cmake_utils/6.1.0#9ced9bcfd95178b35d2ec5955b725a5652dbda26")
+        self.build_requires("cmake_utils/7.0.0#9bf47716aeee70a8dcfc8592831a0318eb327a09")
 
     def requirements(self):
-        self.requires("sdl2/2.0.22")
+        self.requires("sdl2/2.0.22#b126ed43d5b112250d0f20909ca3867f466e28ab")
