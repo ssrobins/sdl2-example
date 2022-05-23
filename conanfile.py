61e3cbef4d7a53fafdd6e1c43b61a9da3d695eb3
Steve Robinson <ssrobins@gmail.com>
2022-05-23 16:17:22 -0700

    Use separate Debug and Release Conan packages (#13)
    * Switch to cmake_find_package_multi generator
    
    * Update product version
    
    * Update Conan packages

--- conanfile.py
+++ conanfile.py
@@ -1,13 +1,13 @@
 from conans import ConanFile
 
 class Conan(ConanFile):
-    settings = "os"
-    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build_requirements(self):
         if self.settings.os == "Android":
             self.build_requires("android_sdl2/2.6.2#da38ed876673a6da88d73f6dcb0138de5561125b")
-        self.build_requires("cmake_utils/6.1.0#9ced9bcfd95178b35d2ec5955b725a5652dbda26")
+        self.build_requires("cmake_utils/7.0.0#9bf47716aeee70a8dcfc8592831a0318eb327a09")
 
     def requirements(self):
-        self.requires("sdl2/2.0.22#b8fc0172efb65d6abc2aad31bda8431f4efaf8b9")
+        self.requires("sdl2/2.0.22#9eef18bc748aef7bfc89e085ee925b18e60741c6")
