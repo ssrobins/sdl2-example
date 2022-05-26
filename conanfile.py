17ad29a0f898c5581b6ad887fcd650c6038d7b94
Steve Robinson <ssrobins@gmail.com>
2022-05-25 19:51:32 -0700

    Fix cmake_utils

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ class Conan(ConanFile):
     def build_requirements(self):
         if self.settings.os == "Android":
             self.build_requires("android_sdl2/2.6.2#da38ed876673a6da88d73f6dcb0138de5561125b")
-        self.build_requires("cmake_utils/9.0.0#f0a11f192f7fad4c048ff5ded59027bc043d7cb7")
+        self.build_requires("cmake_utils/9.0.1#7f745054c87ea0007a89813a4d2c30c4c95e24b2")
 
     def requirements(self):
         self.requires("sdl2/2.0.22#9eef18bc748aef7bfc89e085ee925b18e60741c6")
