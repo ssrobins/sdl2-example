201e3fb5d075742288da9e39e2b3a63bb797478e
Steve Robinson <ssrobins@gmail.com>
2022-06-02 18:36:30 -0700

    Update Conan packages to reference conan-recipes repo sha

--- conanfile.py
+++ conanfile.py
@@ -6,8 +6,8 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires("android_sdl2/2.6.2#da38ed876673a6da88d73f6dcb0138de5561125b")
-        self.build_requires("cmake_utils/9.0.1#7f745054c87ea0007a89813a4d2c30c4c95e24b2")
+            self.build_requires("android_sdl2/2.6.2#e5668aa2be2aa3ef28744dbe2a780c4f7f39a8a8")
+        self.build_requires("cmake_utils/9.0.1#e5668aa2be2aa3ef28744dbe2a780c4f7f39a8a8")
 
     def requirements(self):
-        self.requires("sdl2/2.0.22#c24bc3911c2f1ce3c1b2a07834e78f8d4cdcd735")
+        self.requires("sdl2/2.0.22#e5668aa2be2aa3ef28744dbe2a780c4f7f39a8a8")
