320075c4a7a35eef06c0814a1a15893540607500
Steve Robinson <ssrobins@gmail.com>
2022-06-12 17:53:34 -0700

    Downgrade Android NDK to reclaim compatibility with Android API 16-18

--- conanfile.py
+++ conanfile.py
@@ -6,6 +6,6 @@ class Conan(ConanFile):
 
     def requirements(self):
         if self.settings.os == "Android":
-            self.requires("android_sdl/2.6.2#94f9b35ee28227c1985125702817819f15e76186")
-        self.requires("cmake_utils/10.0.1#94f9b35ee28227c1985125702817819f15e76186")
-        self.requires("sdl/2.0.22#94f9b35ee28227c1985125702817819f15e76186")
+            self.requires("android_sdl/2.6.2#fe249e56a871dd3e09cf9ecbdac13e775463d983")
+        self.requires("cmake_utils/10.0.1#fe249e56a871dd3e09cf9ecbdac13e775463d983")
+        self.requires("sdl/2.0.22#fe249e56a871dd3e09cf9ecbdac13e775463d983")
