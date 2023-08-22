7c8a313b9349b82a28cdb36d45d62ca675d9c265
Steve Robinson <ssrobins@gmail.com>
2023-08-22 16:57:21 -0700

    Transition to public Conan repository & upgrade Xcode (#42)
    * Transition to public Conan repository, ConanCenter, since free-tier Artifactory will soon go away
    
    * Update GitHub Actions caching for Conan, pip, and Gradle packages
    
    * Move android_sdl and cmake_utils files into the repo
    
    * Upgrade to Xcode 14 and drop unsupported macOS and iOS versions

--- conanfile.py
+++ conanfile.py
@@ -5,9 +5,39 @@ required_conan_version = ">=2.0.4"
 class Conan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeDeps"
+    default_options = {
+        "sdl/*:alsa": False,
+        "sdl/*:arts": False,
+        "sdl/*:directfb": False,
+        "sdl/*:directx": False,
+        "sdl/*:esd": False,
+        "sdl/*:iconv": False,
+        "sdl/*:jack": False,
+        "sdl/*:libunwind": False,
+        "sdl/*:nas": False,
+        "sdl/*:opengl": False,
+        "sdl/*:opengles": False,
+        "sdl/*:pulse": False,
+        "sdl/*:sndio": False,
+        "sdl/*:video_rpi": False,
+        "sdl/*:vulkan": False,
+        "sdl/*:wayland": False,
+        "sdl/*:x11": False,
+        "sdl/*:xcursor": False,
+        "sdl/*:xinerama": False,
+        "sdl/*:xinput": False,
+        "sdl/*:xrandr": False,
+        "sdl/*:xscrnsaver": False,
+        "sdl/*:xshape": False,
+        "sdl/*:xvm": False
+    }
+
+    def config_options(self):
+        self.options["sdl/*"].iconv = self.settings.os == "iOS"
+        self.options["sdl/*"].opengl = self.settings.os != "Android"
+        self.options["sdl/*"].opengles = self.settings.os == "Android"
+        self.options["sdl/*"].x11 = self.settings.os == "Linux"
+        self.options["sdl/*"].xinput = self.settings.os == "Linux"
 
     def requirements(self):
-        if self.settings.os == "Android":
-            self.requires("android_sdl/2.7.6@ssrobins")
-        self.requires("cmake_utils/12.0.1@ssrobins")
-        self.requires("sdl/2.26.5@ssrobins")
+        self.requires("sdl/2.26.5")
