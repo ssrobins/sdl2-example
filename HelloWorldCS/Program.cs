65c5fa3df273c10177d1238503071efd5fa7f2fa
Steve Robinson <ssrobins@gmail.com>
2025-12-06 13:06:57 -0800

    Add 'hello world' C# project

---
+++ HelloWorldCS/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorldCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from C#!");
+        }
+    }
+}
